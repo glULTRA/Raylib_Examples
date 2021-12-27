07e7ce2a5636e0055f27dc8fca4a631d63a08187
glULTRA <7amakurd2002@gmail.com>
2021-12-27 16:03:34 +0300

    Added Draw Sphere Wires Example

--- examples/drawSphereWires.cpp
+++ examples/drawSphereWires.cpp
@@ -1,13 +1,4 @@
-#include <iostream>
-#include <cmath>
-#include <vector>
 #include <raylib.h>
-
-#if defined(_WIN32)
-    #define GLSL_VERSION 330
-#else
-    #define GLSL_VERSION 100
-#endif
 
 #define SCR_WIDTH 612
 #define SCR_HEIGHT 612
