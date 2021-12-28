2a60dd230348ad35e6c28721059d36861edfdb53
glULTRA <7amakurd2002@gmail.com>
2021-12-28 17:53:13 +0300

    Added Model 2

--- main.cpp
+++ main.cpp
@@ -25,9 +25,7 @@ int main()
 
     // Model
     Model model = Model{1.0f};
-    Model model2 = Model{1.0f};
-    model = LoadModel("examples/res/Object/Model1/gltf/rigged_figure.glb");
-    model2 = LoadModel("examples/res/Object/Model1/gltf/vertex_colored_object.glb");
+    model = LoadModel("examples/res/Object/Model2/watermill.obj");
 
 
     // FPS
@@ -38,27 +36,13 @@ int main()
         /* <---------Update---------> */
         float time = GetTime();
         UpdateCamera(&camera);
-        model.transform      = MatrixRotate(Vector3{1.0f, 0.0f, 0.0f}, 3.0f * time);
-        model2.transform     = MatrixScale(0.5f,0.5f,0.5f);
-        model2.transform     = MatrixMultiply(model2.transform ,MatrixRotate(Vector3{1.0f, 0.0f, 0.0f}, 3.0f * time));
-        model2.transform     = MatrixMultiply(model2.transform ,MatrixTranslate(0.0f, 0.0f, std::sin(time)));
-
-        static int slice = 10;
-        static float space = 1.0f;
-
-        if(IsKeyPressed(KEY_UP)) slice++;
-        else if(IsKeyPressed(KEY_DOWN)) slice--;
-
-        if(IsKeyPressed(KEY_W)) space+=0.1f;
-        else if(IsKeyPressed(KEY_S)) space-=0.1f;
 
         /* <---- Render ----> */
         BeginDrawing();
             ClearBackground(BLACK);
             BeginMode3D(camera);
-                DrawGrid(slice, space);
-                DrawModel(model, Vector3{0.0f, 0.0f, 0.0f}, 1.0f, WHITE);
-                DrawModel(model2, Vector3{2.0f, 0.0f, -1.0f}, 1.0f, WHITE);
+                DrawGrid(10, 1.0f);
+                DrawModel(model, Vector3{0.0f, 0.0f, 0.0f}, 0.3f, WHITE);
             EndMode3D();
         EndDrawing();
     }
