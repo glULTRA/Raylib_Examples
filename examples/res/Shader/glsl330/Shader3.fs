dd638ed23a91405eb968525071b27283866acb86
glULTRA <7amakurd2002@gmail.com>
2021-12-29 14:41:35 +0300

    Updated Shader 3

--- examples/res/Shader/glsl330/Shader3.fs
+++ examples/res/Shader/glsl330/Shader3.fs
@@ -1,17 +1,15 @@
-#version 330 
+#version 330
 
-out vec4 finalColor;
 
+out vec4 FragColor;
+
+in vec4 fragColor;
 in vec2 fragTexCoord;
-in vec3 fragColor;
 
 uniform sampler2D texture0;
-uniform vec4 colDiffuse;
+uniform float xColor; 
 
-void main(){
-    vec4 texelColor = texture(texture0, fragTexCoord);
-
-    float gray = dot(texelColor.rgb, vec3(0.299, 0.587, 0.114));
-    
-    finalColor = vec4(gray ,gray ,gray ,texelColor);
+void main()
+{
+    FragColor = texture(texture0, fragTexCoord) * vec4(fragColor.x * xColor, fragColor.y, fragColor.z, fragColor.w);
 }
