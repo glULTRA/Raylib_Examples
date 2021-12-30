272436484e65bc05a984e548aa5d67083dd11b12
glULTRA <7amakurd2002@gmail.com>
2021-12-30 10:07:32 +0300

    Added Mandelbrot Shader

--- main.cpp
+++ main.cpp
@@ -15,40 +15,6 @@
 #define SCR_WIDTH 612
 #define SCR_HEIGHT 612
 
-typedef struct ULTHeightMap{
-    Image image;
-    Texture texture;
-    Mesh mesh;
-    Model model;
-    Shader shader;
-} HeightMap;
-
-HeightMap ChooseHeightMap(const char* path){
-    // Texture
-    Image image = LoadImage(path);
-    Texture2D texture = LoadTextureFromImage(image);
-    // Shader
-    Shader shader = LoadShader(0, TextFormat("examples/res/Shader/glsl%i/Shader3.fs", GLSL_VERSION));
-
-    // Mesh
-    Mesh mesh = GenMeshHeightmap(image, Vector3{1.0f, 1.0f, 1.0f});
-    
-    // Model
-    Model model = LoadModelFromMesh(mesh);
-    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
-    model.materials[0].shader = shader;
-
-    HeightMap heightmap;
-    heightmap.image = image;
-    heightmap.texture = texture;
-    heightmap.mesh = mesh;
-    heightmap.model = model;
-    heightmap.shader = shader;
-
-    UnloadImage(image);
-
-    return heightmap;
-}
 
 int main()
 {
@@ -56,13 +22,8 @@ int main()
     SetConfigFlags(FLAG_MSAA_4X_HINT);      // Enable Multi Sampling Anti Aliasing 4x (if available)
     InitWindow(SCR_WIDTH, SCR_HEIGHT, "Raylib");
 
-    // Camera
-    Camera3D camera = Camera3D{ { 0.0f, 10.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
-    SetCameraMode(camera, CAMERA_ORBITAL);
-
-    // HeightMap
-    int choose = 0;
-    HeightMap heightmap = ChooseHeightMap("examples/res/Texture/heightmap.png");
+    // Shader
+    Shader shader = LoadShader(0, TextFormat("examples/res/Shader/glsl%i/Mandelbrot.fs", GLSL_VERSION));
 
     // FPS
     SetTargetFPS(60);
@@ -71,52 +32,13 @@ int main()
     {
         /* <---------Update---------> */
         float time = GetTime();
-        UpdateCamera(&camera);
-        static int prevChoose = choose;
-        static float xValue = 0.0f;
-        xValue = (std::sin(time) / 2.0f) + 0.5f;
-
-        if(choose == 0 && prevChoose != choose ){
-            heightmap = ChooseHeightMap("examples/res/Texture/heightmap.png");
-            prevChoose = choose;
-        }
-        else if(choose == 1 && prevChoose != choose){
-            heightmap = ChooseHeightMap("examples/res/Texture/heightmap2.png");
-            prevChoose = choose;
-        }else if(choose == 2 && prevChoose != choose){
-            heightmap = ChooseHeightMap("examples/res/Texture/heightmap3.png");
-            prevChoose = choose;
-        }else if(choose == 3 && prevChoose != choose){
-            heightmap = ChooseHeightMap("examples/res/Texture/heightmap4.png");
-            prevChoose = choose;
-        }else if(choose == 4 && prevChoose != choose){
-            heightmap = ChooseHeightMap("examples/res/Texture/heightmap5.png");
-            heightmap.model.transform = MatrixMultiply(heightmap.model.transform,MatrixRotate(Vector3{1.0f, 0.0f, 0.0f}, 240.0f));
-            heightmap.model.transform = MatrixMultiply(heightmap.model.transform,MatrixTranslate(0.0f, 1.0f, 0.0f));
-            prevChoose = choose;
-        }
 
         /* <---- Render ----> */
         BeginDrawing();
             ClearBackground(WHITE);
-            BeginMode3D(camera);
-                DrawGrid(10, 1.0f);
-                BeginShaderMode(heightmap.shader);
-                    DrawModel(heightmap.model, Vector3{-3.0f, 0.0f, -3.0f}, 5.0f, WHITE);
-                    SetShaderValue(heightmap.shader, GetShaderLocation(heightmap.shader, "xColor"), &xValue, SHADER_UNIFORM_FLOAT);
-                EndShaderMode();
-            EndMode3D();
-            //DrawTexture(texture, SCR_WIDTH - texture.width, 50, WHITE);
-            if(GuiButton(Rectangle{500.0f, 50.0f, 70.0f ,30.0f}, "HeightMap1"))
-                choose = 0;
-            else if(GuiButton(Rectangle{500.0f, 80.0f, 70.0f ,30.0f}, "HeightMap2"))
-                choose = 1;
-            else if(GuiButton(Rectangle{500.0f, 110.0f, 70.0f ,30.0f}, "HeightMap3"))
-                choose = 2;
-            else if(GuiButton(Rectangle{500.0f, 140.0f, 70.0f ,30.0f}, "HeightMap4"))
-                choose = 3;
-            else if(GuiButton(Rectangle{500.0f, 170.0f, 70.0f ,30.0f}, "HeightMap5"))
-                choose = 4;
+            BeginShaderMode(shader);
+
+            EndShaderMode();
         EndDrawing();
     }
 
